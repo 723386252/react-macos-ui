612638d999dd140ca5851781a326e9958b327b8b
sheweijie <723386252@qq.com>
2024-12-18 22:44:13 +0800

    styles 将css内联进js

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,13 @@ import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 import { resolve } from 'path';
 import dts from 'vite-plugin-dts';
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    cssInjectedByJsPlugin(),
     svgr({
       include: "**/*.svg?react"
     }),
